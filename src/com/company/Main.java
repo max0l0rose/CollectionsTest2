0d5f6436756ee5db92b1deca53c15117ca4377e8
9956799@gmail.com <Qqq_12345>
2021-10-28 21:39:48 +0300

    IntStream.range(1, 50).collect(ArrayList::new, List::add, List::addAll)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,31 @@ package com.company;
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException
+    {
+
+        Collection<Integer> syncCollection = Collections.synchronizedCollection(
+                new ArrayList<>()
+        );
+        Runnable listOperations = () -> {
+            syncCollection.addAll(IntStream.range(1, 50).collect(ArrayList::new, List::add, List::addAll));
+                    //Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20));
+        };
+
+        Thread thread1 = new Thread(listOperations);
+        Thread thread2 = new Thread(listOperations);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+
+        //assertThat(syncCollection.size()).isEqualTo(12);
+
 //        HashSet<Object> set = new HashSet<>();
 //        Object o = new Object();
 //        set.add(o);
@@ -20,9 +41,8 @@ public class Main {
         CopyOnWriteArrayList copyOnWriteArrayList;
 
 
-        Collection<Integer> syncCollection = Collections.synchronizedCollection(new ArrayList<>());
-
-        List<Integer> syncList = Collections.synchronizedList(new ArrayList<>());
+//        Collection<Integer> syncCollection = Collections.synchronizedCollection(new ArrayList<>());
+//        List<Integer> syncList = Collections.synchronizedList(new ArrayList<>());
 
         //TimeUnit.MINUTES.sleep(1);
     }
